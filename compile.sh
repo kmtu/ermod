d6526f6a10b849051124554d5a5ef437bea8214d
Shun Sakuraba <shun.sakuraba@gmail.com>
2010-04-07 17:06:45 +0900

    New compilation script

--- compile.sh
+++ compile.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-F90FLAGS="-O3 -xP -mcmodel=large -autodouble -extend-source -shared-intel"
-mpiifort -g -o enganal.o -c enganal.f $F90FLAGS -cpp -Dtrjctry -DMARBLE -DMKL 
+F90FLAGS="-O3 -xP -mcmodel=large -autodouble -extend-source -shared-intel -fomit-frame-pointer"
+mpiifort -static -g -o energromacs enganal.f $F90FLAGS -cpp -Dtrjctry -DGROMACS -DMKL /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide -L$HOME/lib
+mpiifort -static -g -o enernamd enganal.f $F90FLAGS -cpp -Dtrjctry -DNAMD -DMKL /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide -L$HOME/lib
+#mpiifort -g -o enganal.o -c enganal.f $F90FLAGS -cpp -Dtrjctry -DMARBLE -DMKL 
 #mpiifort -o insertion.o -c insertion.f $F90FLAGS -cpp -Dtrjctry -DMARBLE -DMKL 
 #mpiifort -o setconf.o -c setconf.f $F90FLAGS -cpp -Dtrjctry -DMARBLE -DMKL 
-mpiifort -g -o enermarble enganal.o /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide
-mpiifort -g -o enganal.o -c enganal.f $F90FLAGS -cpp -Dtrjctry -DGROMACS -DMKL 
-mpiifort -g -o energromacs enganal.o /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide
-mpiifort -g -o enganal.o -c enganal.f $F90FLAGS -cpp -Dtrjctry -DNAMD -DMKL 
-mpiifort -g -o enernamd enganal.o /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide
+#mpiifort -g -o enermarble enganal.o /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide -L$HOME/lib -lprofiler
+#mpiifort -g -o enganal.o -c enganal.f $F90FLAGS -cpp -Dtrjctry -DGROMACS -DMKL 
+#mpiifort -g -o energromacs enganal.o /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide -L$HOME/lib -lprofiler
+#mpiifort -g -o enganal.o -c enganal.f $F90FLAGS -cpp -Dtrjctry -DNAMD -DMKL 
+#mpiifort -g -o enernamd enganal.o /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide -L$HOME/lib -lprofiler
 #mpiifort -o energromacs -O3 -xP -fpic -shared-intel -mcmodel=large –autodouble -extend-source -cpp -Dtrjctry -DGROMACS -DMKL /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 enganal.f insertion.f setconf.f -lmkl_em64t -lguide -L/usr/lib –lpthread
 #mpiifort -o enernamd -O3 -xP -fpic -shared-intel -mcmodel=large -autodouble -extend-source -cpp -Dtrjctry -DNAMD -DMKL /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 enganal.f insertion.f setconf.f -lmkl_em64t -lguide -L/usr/lib –lpthread
