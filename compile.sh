325850597409e12e203591c69ec397e4eb942163
Shun Sakuraba <shun.sakuraba@gmail.com>
2010-04-16 17:53:45 +0900

    Changed default FFT library to FFTW

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@
 
 F90FLAGS_noOPT="-g -traceback -xP -mcmodel=large -autodouble -extend-source -shared-intel -fomit-frame-pointer"
 F90FLAGS="$F90FLAGS_noOPT -O3"
-FFTFLAGS="-DMKL"
-FFTLIBS="/opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide"
 #FFTFLAGS="-DMKL"
 #FFTLIBS="/opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide"
+FFTFLAGS="-DFFTW"
+FFTLIBS="-L$HOME/lib -lfftw3"
 mpiifort -warn all -static -o energromacs spline.f90 fft_iface.f90 enganal.f $F90FLAGS -cpp -Dtrjctry -DGROMACS ${FFTFLAGS} ${FFTLIBS} || exit 1
 mpiifort -warn all -o energromacs_p spline.f90 fft_iface.f90 enganal.f $F90FLAGS -cpp -Dtrjctry -DGROMACS ${FFTFLAGS} ${FFTLIBS} -L$HOME/lib -lprofiler || exit 1
 mpiifort -warn all -o energromacs_noMPI spline.f90 fft_iface.f90 enganal.f $F90FLAGS_noOPT -cpp -O0 -Dtrjctry -DGROMACS -DMKL -DnoMPI /opt/intel/mkl/10.0.011/include/mkl_dfti.f90 -lmkl_em64t -lguide -L$HOME/lib -lprofiler || exit 1
