18a01c5e7d98882efe4184b5fe402160efeb7e9f
Shun Sakuraba <none@none>
2011-08-26 00:19:44 +0900

    Fixed MKL interface
    --HG--
    branch : refactor

--- fft_iface.f90
+++ fft_iface.f90
@@ -19,12 +19,7 @@ module fft_iface
 #endif
   end type fft_handle
 
-#ifdef MKL
-  type(Dfti_Descriptor), pointer :: desc_ctc, desc_c, desc_rtc
-#endif
 #ifdef FFTW
-  ! unsupported
-  integer(8) :: plan_ctc, plan_ctc_backward, plan_inplace, plan_r2c_inplace, plan_ctr, plan_rtc
 #include "fftw3.f"
 #endif
   
@@ -34,155 +29,85 @@ contains
     integer, intent(in) :: fftsize_in(3)  
     fftsize(:) = fftsize_in(:)
   end subroutine fft_set_size
+
 #ifdef MKL
   ! 3D-FFT, MKL version
   
-  ! initialize FFT, complex to complex, out-of-place
-  subroutine fft_init_ctc(in, out)
+  ! initialize FFT, real to complex, out-of-place
+  subroutine fft_init_rtc(handle, in, out)
     use MKL_DFTI
+    type(fft_handle), intent(out) :: handle
+    real, intent(inout) :: in(fftsize(1), fftsize(2), fftsize(3))
+    complex, intent(inout) :: out(fftsize(1)/2+1, fftsize(2), fftsize(3))
     integer :: stat
-    complex :: in(fftsize(1), fftsize(2), fftsize(3)), out(fftsize(1), fftsize(2), fftsize(3))
     real :: dummy
     if(kind(dummy) == 8) then
-       stat = DftiCreateDescriptor(desc_ctc, DFTI_DOUBLE, DFTI_COMPLEX, 3, fftsize)
+       stat = DftiCreateDescriptor(handle%desc, DFTI_DOUBLE, DFTI_REAL, 3, fftsize)
     else
-       stat = DftiCreateDescriptor(desc_ctc, DFTI_FLOAT, DFTI_COMPLEX, 3, fftsize)
+       stat = DftiCreateDescriptor(handle%desc, DFTI_FLOAT, DFTI_REAL, 3, fftsize)
     endif
     if(stat /= 0) stop "MKL-FFT: failed to execute DftiCreateDescriptor"
-    stat = DftiSetValue(desc_ctc, DFTI_PLACEMENT, DFTI_NOT_INPLACE)
+    stat = DftiSetValue(handle%desc, DFTI_PLACEMENT, DFTI_NOT_INPLACE)
     if(stat /= 0) stop "MKL-FFT: failed to set DFTI_NOT_INPLACE"
-    stat = DftiCommitDescriptor(desc_ctc)
+    stat = DftiSetValue(handle%desc, DFTI_PACKED_FORMAT, DFTI_CCE_FORMAT)
+    if(stat /= 0) stop "MKL-FFT: failed to set DFTI_PACKED_FORMAT"
+    stat = DftiCommitDescriptor(handle%desc)
     if(stat /= 0) stop "MKL-FFT: failed to execute DftiCommitDescriptor"
-  end subroutine fft_init_ctc
-
-  subroutine fft_init_ctc_backward(in, out)
-    complex :: in(fftsize(1), fftsize(2), fftsize(3)), out(fftsize(1), fftsize(2), fftsize(3))
-    call fft_init_ctc(in, out)
-  end subroutine fft_init_ctc_backward
-  
-  subroutine fft_init_inplace(inout)
-    use MKL_DFTI
-    integer :: stat
-    complex :: inout(fftsize(1), fftsize(2), fftsize(3))
-    real :: dummy
-    if(kind(dummy) == 8) then
-       stat = DftiCreateDescriptor(desc_c, DFTI_DOUBLE, DFTI_COMPLEX, 3, fftsize)
-    else
-       stat = DftiCreateDescriptor(desc_c, DFTI_FLOAT, DFTI_COMPLEX, 3, fftsize)
-    endif
-    if(stat /= 0) stop "MKL-FFT: failed to execute DftiCreateDescriptor"
-    stat = DftiCommitDescriptor(desc_c)
-    if(stat /= 0) stop "MKL-FFT: failed to execute DftiCommitDescriptor"
-  end subroutine fft_init_inplace
-
-  subroutine fft_init_rtc(in, out)
-    use MKL_DFTI
-    integer :: stat
-    real :: in(fftsize(1), fftsize(2), fftsize(3))
-    complex :: out(fftsize(1) / 2 + 1, fftsize(2) / 2 + 1, fftsize(3) / 2 + 1)
-    real :: dummy
-    if(kind(dummy) == 8) then
-       stat = DftiCreateDescriptor(desc_rtc, DFTI_DOUBLE, DFTI_REAL, 3, fftsize)
-    else
-       stat = DftiCreateDescriptor(desc_rtc, DFTI_FLOAT, DFTI_REAL, 3, fftsize)
-    end if
-    if(stat /= 0) stop "MKL-FFT: failed to execute DftiCreateDescriptor"
-    stat = DftiSetValue(desc_rtc, DFTI_PLACEMENT, DFTI_NOT_INPLACE)
-    if(stat /= 0) stop "MKL-FFT: failed to set DFTI_NOT_INPLACE"
-    stat = DftiCommitDescriptor(desc_rtc)
-    if(stat /= 0) stop "MKL-FFT: failed to execute DftiCreateDescriptor"
   end subroutine fft_init_rtc
 
-  ! Should be BACKWARD
-!   subroutine fft_init_ctr(fftsize, in, out)
-!     use MKL_DFTI
-!     integer :: stat, fftsize(3)
-!     complex(8) :: in(:)
-!     real(8) :: out(:)
-!     stat = DftiCreateDescriptor(desc_ctr, DFTI_DOUBLE, DFTI_COMPLEX_REAL, 3, fftsize)
-!     if(stat /= 0) stop "MKL-FFT: failed to execute DftiCreateDescriptor"
-!     stat = DftiSetValue(desc_ctr, DFTI_PLACEMENT, DFTI_NOT_INPLACE)
-!     if(stat /= 0) stop "MKL-FFT: failed to set DFTI_NOT_INPLACE"
-!     stat = DftiCommitDescriptor(desc_ctr)
-!     if(stat /= 0) stop "MKL-FFT: failed to execute DftiCreateDescriptor"
-!   end subroutine fft_init_ctr
-
-  ! complex-to-complex, out-of-place FFT
-  subroutine fft_ctc(in, out)
-    use MKL_DFTI
-    integer :: stat
-    ! use fortran77-style size to bypass type-check. Note that this invalidates type-check!
-    complex, intent(in) :: in(*)
-    complex, intent(out) :: out(*)
-    stat = DftiComputeForward(desc_ctc, in, out)
-    if(stat /= 0) stop "MKL-FFT: failed to execute compute_forward_z_out (oops!)"
-  end subroutine fft_ctc
-
-  ! complex-to-complex, out-of-place FFT, backward
-  subroutine fft_ctc_backward(in, out)
-    use MKL_DFTI
-    integer :: stat
-    ! use fortran77-style size to bypass type-check. Note that this invalidates type-check!
-    complex, intent(in) :: in(*)
-    complex, intent(out) :: out(*)
-    stat = DftiComputeBackward(desc_ctc, in, out)
-    if(stat /= 0) stop "MKL-FFT: failed to execute compute_forward_z_out (oops!)"
-  end subroutine fft_ctc_backward
-
-  ! complex-to-complex, in-place FFT
-  subroutine fft_inplace(inout)
-    use MKL_DFTI
-    integer :: stat
-    complex, intent(inout) :: inout(*)
-    stat = DftiComputeForward(desc_c, inout)
-    if(stat /= 0) stop "MKL-FFT: failed to execute compute_forward_z (oops!)"
-  end subroutine fft_inplace
+  ! real to complex. Note that MKL's DFT interface is exactly the same for r2c and c2r, thus just pass the argument.
+  subroutine fft_init_ctr(handle, in, out)
+    type(fft_handle), intent(out) :: handle
+    complex, intent(inout) :: in(fftsize(1)/2+1, fftsize(2), fftsize(3))
+    real, intent(inout) :: out(fftsize(1), fftsize(2), fftsize(3))
+    call fft_init_rtc(handle, out, in)
+  end subroutine fft_init_ctr
 
   ! real-to-complex, out-of-place FFT
-  subroutine fft_rtc(in, out)
+  subroutine fft_rtc(handle, in, out)
     use MKL_DFTI
-    integer :: stat
+    type(fft_handle), intent(in) :: handle
+    ! use fortran77-style size to bypass type-check. Note that this invalidates type-check!
     real, intent(in) :: in(*)
     complex, intent(out) :: out(*)
-    stat = DftiComputeForward(desc_rtc, in, out)
-    if(stat /= 0) stop "MKL-FFT: failed to execute compute_forward_dz (oops!)"
+
+    integer :: stat
+
+    stat = DftiComputeForward(handle%desc, in, out)
+    if(stat /= 0) stop "MKL-FFT: failed to execute DftiComputeForward @ rtc (oops!)"
   end subroutine fft_rtc
 
-!   ! complex-to-real, out-of-place FFT
-!   subroutine fft_ctr(in, out)
-!     use MKL_DFTI
-!     integer :: stat
-!     complex(8), intent(in) :: in(:, :, :)
-!     real(8), intent(out) :: out(:, :, :)
-!     stat = dfti_compute_forward_z_out_d(desc_ctc, in, out)
-!   end subroutine fft_ctr
+  ! complex-to-real, out-of-place FFT
+  subroutine fft_ctr(handle, in, out)
+    use MKL_DFTI
+    type(fft_handle), intent(in) :: handle
+    ! use fortran77-style size to bypass type-check. Note that this invalidates type-check!
+    complex, intent(in) :: in(*)
+    real, intent(out) :: out(*)
+
+    integer :: stat
+
+    stat = DftiComputeBackward(handle%desc, in, out)
+    if(stat /= 0) stop "MKL-FFT: failed to execute DftiComputeForward @ ctr (oops!)"
+  end subroutine fft_ctr
 
   ! clean-up fft handle
-  subroutine fft_cleanup_ctc()
-    integer :: stat
-    stat = DftiFreeDescriptor(desc_ctc)
-    if(stat /= 0) stop "MKL-FFT: failed to execute DftiFreeDescriptor (oops!)"
-  end subroutine fft_cleanup_ctc
+  subroutine fft_cleanup_rtc(handle)
+    type(fft_handle), intent(in) :: handle
 
-  ! clean-up fft handle
-  subroutine fft_cleanup_inplace()
     integer :: stat
-    stat = DftiFreeDescriptor(desc_c)
-    if(stat /= 0) stop "MKL-FFT: failed to execute DftiFreeDescriptor (oops!)"
-  end subroutine fft_cleanup_inplace
-
-  ! clean-up fft handle
-  subroutine fft_cleanup_rtc()
-    integer :: stat
-    stat = DftiFreeDescriptor(desc_rtc)
+    stat = DftiFreeDescriptor(handle%desc)
     if(stat /= 0) stop "MKL-FFT: failed to execute DftiFreeDescriptor (oops!)"
   end subroutine fft_cleanup_rtc
 
-!   ! clean-up fft handle
-!   subroutine fft_cleanup_ctr()
-!     stat = DftiFreeDescriptor(desc_ctr)
-!     if(stat /= 0) stop "MKL-FFT: failed to execute DftiFreeDescriptor (oops!)"
-!   end subroutine fft_cleanup_ctr
+   ! clean-up fft handle
+  subroutine fft_cleanup_ctr(handle)
+    type(fft_handle), intent(in) :: handle
+
+    integer :: stat
+    stat = DftiFreeDescriptor(handle%desc)
+    if(stat /= 0) stop "MKL-FFT: failed to execute DftiFreeDescriptor (oops!)"
+  end subroutine fft_cleanup_ctr
 
 #endif
 #ifdef FFTW
