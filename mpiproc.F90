36445b7db294ecb4214a6c97115a22e4a7311885
Shun Sakuraba <none@none>
2011-09-30 16:10:28 +0900

    Warning message fix
    --HG--
    branch : refactor

--- mpiproc.F90
+++ mpiproc.F90
@@ -194,11 +194,13 @@ contains
     use engmain, only: stdout, force_calculation
     implicit none
     character(len=4), intent(in) :: typ
-    if(typ == 'mbin') write(stdout, '(A)'), " Warning: the maximum bin coordinate is too low for this species"
+    if(typ == 'mbin') write(stdout, '(A)'), " Warning: the maximum binning energy is too low for this species"
     if(typ == 'insu') write(stdout, '(A)'), " Warning: unique pair of snapshots is smaller than expected"
     if(force_calculation) return
-    write(stdout, '(A)'), "The program aborts becaue there is a warning"
-    write(stdout, '(A)'), "If you wish to force program running, specify 'force_calculation = .true.' in parameters_er."
+    write(stdout, '(A)') "The program aborts becaue there is a warning"
+    write(stdout, '(A,A)') "If you wish to force program running, specify 'force_calculation = .true.' in parameters_er, ", &
+         "at &ene_param section."
+
     call mpi_abend()
     stop
   end subroutine warning
