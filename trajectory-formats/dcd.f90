5b346a64cbf466816dbdb2e14629d93c57a620d9
Shun Sakuraba <none@none>
2011-12-08 21:00:44 +0900

    Fixed problem that deallocation does not occur when error has happened
    --HG--
    branch : refactor

--- trajectory-formats/dcd.f90
+++ trajectory-formats/dcd.f90
@@ -51,21 +51,22 @@ contains
     real(4), allocatable :: buffer(:)
 
     cell(:, :) = 0.
+    allocate(buffer(natom))
+
     if(is_periodic) then
        if(.not. htraj%have_cell_info) stop "Cell info is requested, but does not exist!"
        read(htraj%iohandle, err=999) cell(1,1), cell(1,2), cell(2,2), cell(1,3), cell(2,3), cell(3,3)
     end if
 
-    allocate(buffer(natom))
     read(htraj%iohandle, err=999) buffer(:)
     crd(1, :) = buffer(:)
     read(htraj%iohandle, err=999) buffer(:)
-    crd(2, :) = buffer
+    crd(2, :) = buffer(:)
     read(htraj%iohandle, err=999) buffer(:)
-    crd(3, :) = buffer
-    deallocate(buffer)
+    crd(3, :) = buffer(:)
 
     status = 0
+    deallocate(buffer)
     return
 
 999 status = 1
