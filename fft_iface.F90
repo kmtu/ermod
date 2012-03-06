4efe8c149928d54928b8ead9572ae1af4f798850
Shun Sakuraba <none@none>
2012-03-06 19:41:00 +0900

    Fixed problem that dfftw_init_threads are referenced when OpenMP is not enabled
    --HG--
    branch : ver03

--- fft_iface.F90
+++ fft_iface.F90
@@ -29,7 +29,7 @@ contains
   subroutine fft_set_size(fftsize_in)
 #ifdef FFTW
     !$ use omp_lib, only: omp_get_num_threads
-    external dfftw_init_threads
+    !$ external dfftw_init_threads
 #endif
     integer, intent(in) :: fftsize_in(3)  
     fftsize(:) = fftsize_in(:)
